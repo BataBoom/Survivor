563e1bbd4392ed838f2bc12942d8de2183ac811d
BataBoom <contact@bataboom.dev>
2024-05-19 13:26:25 +0000

    Pickem V2 first commit, introducing multiple pools, cleaner UI

---
+++ app/Livewire/Traits/SurvivorTrait.php
@@ -0,0 +1,116 @@
+<?php
+
+namespace App\Livewire\Traits;
+
+use App\Models\WagerQuestion;
+use App\Models\WagerOption;
+use App\Models\WagerTeam;
+use Illuminate\Support\Collection;
+use DateTimeZone;
+use Carbon\Carbon;
+
+trait SurvivorTrait
+{
+    public function allGames($week)
+    {
+
+        $ScheduleIds = WagerQuestion::where('week', $week)->pluck('game_id')->toArray();
+        $Teams = WagerOption::WhereIn('game_id', $ScheduleIds)->get();
+        $Games = WagerQuestion::WhereIn('game_id', $ScheduleIds)->get();
+
+        $options = collect();
+        foreach ($Games as $game) {
+            $teamIds = $game->gameoptions()->pluck('team_id');
+            $teamInfo = WagerTeam::whereIn('team_id', $teamIds)->select('abbreviation', 'name','team_id')->get();
+
+            $combinedData = collect([
+                'game' => $game->question,
+                'starts' => $game->starts_at,
+                'gid' => $game->game_id,
+                'mid' => $game->id,
+                'info' => $teamInfo,
+
+            ]);
+            $options->push($combinedData);
+        }
+        return [$Games, $options->toArray()];
+    }
+
+    public function pickemGames($week)
+    {
+
+        $games = WagerQuestion::Where('week', $week)->get();
+        $combinedData = collect();
+        $keys = $games->pluck('game_id');
+        foreach ($games as $game) {
+            $teamIds = $game->gameoptions()->pluck('team_id');
+            $teamInfo = WagerTeam::whereIn('team_id', $teamIds)->select('abbreviation', 'name', 'team_id', 'color', 'altColor')->get();
+
+            $item = (object)[
+                'game' => $game->question,
+                'starts' => $game->starts_at,
+                'gid' => $game->game_id,
+                'mid' => $game->id,
+                'info' => $teamInfo,
+                'result' => $game->result,
+            ];
+
+            $combinedData->push($item);
+        }
+
+        $combined = collect($keys)->combine($combinedData);
+
+        return $combined;
+    }
+
+
+    public function decipherWeek()
+    {
+
+        $dateRanges = [
+            ['start' => '2024-09-08', 'end' => '2024-09-12'],
+            ['start' => '2024-09-13', 'end' => '2024-09-18'],
+            ['start' => '2024-09-19', 'end' => '2024-09-25'],
+            ['start' => '2024-09-26', 'end' => '2024-10-02'],
+            ['start' => '2024-10-03', 'end' => '2024-10-09'],
+            ['start' => '2024-10-10', 'end' => '2024-10-16'],
+            ['start' => '2024-10-17', 'end' => '2024-10-21'],
+            ['start' => '2024-10-22', 'end' => '2024-10-28'],
+            ['start' => '2024-10-29', 'end' => '2024-11-06'],
+            ['start' => '2024-11-07', 'end' => '2024-11-13'],
+            ['start' => '2024-11-14', 'end' => '2024-11-20'],
+            ['start' => '2024-11-21', 'end' => '2024-11-27'],
+            ['start' => '2024-11-28', 'end' => '2024-12-04'],
+            ['start' => '2024-12-05', 'end' => '2024-12-11'],
+            ['start' => '2024-12-12', 'end' => '2024-12-19'],
+            ['start' => '2024-12-20', 'end' => '2024-12-25'],
+            ['start' => '2024-12-26', 'end' => '2024-12-31'],
+            ['start' => '2025-01-01', 'end' => '2025-01-06'],
+        ];
+
+        $now = date('Y-m-d'); // Current date, can be customized
+        $week = 1;
+        foreach ($dateRanges as $i => $range) {
+            if ($now >= $dateRanges[$i]['start'] && $now <= $dateRanges[$i]['end']) {
+                $week = $i + 1;
+            }
+        }
+
+        return $week;
+    }
+
+
+    public function isLive($week, $pick): bool
+    {
+        //return false;
+        $currentTimeEST = Carbon::now(new DateTimeZone('America/New_York'))->subMinutes(30);
+
+        $locateSelection = WagerOption::with('question')
+            ->where('week', $week)
+            ->where('option', $pick)
+            ->first();
+
+        return $currentTimeEST->lessThan($locateSelection->question->starts_at->subMinutes(30));
+
+    }
+}
---
+++ app/Models/Pickem.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
+
+class Pickem extends Model
+{
+    use HasFactory;
+    protected $table = 'pickem';
+
+    protected $guarded = [];
+
+    protected $casts = [];
+
+    public $timestamps = true;
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+    /* Testing a diff approach vs Survivor, could prove to be better */
+    public function pool()
+    {
+        return $this->belongsTo(SurvivorRegistration::class, 'ticket_id');
+    }
+
+    public function pick()
+    {
+        return $this->hasOne(WagerOption::class, 'team_id', 'selection_id');
+    }
+
+    public function question()
+    {
+        return $this->hasOne(WagerQuestion::class, 'game_id', 'game_id');
+    }
+
+    public function result()
+    {
+        return $this->hasOne(WagerResult::class, 'game_id', 'game_id');
+    }
+
+    public static function getGradedPicksByUser(User $user)
+    {
+        return self::where('user_id', $user->id)
+            ->whereNotNull('result')
+            ->where('week', $week)
+            ->latest()
+            ->get();
+    }
+
+    public static function getNonGradedPicksByUser(User $user, int $week)
+    {
+        return self::where('user_id', $user->id)
+            ->whereNull('result')
+            ->where('week', $week)
+            ->latest()
+            ->get();
+    }
+    public static function scopeMostForGame($query, $gameID)
+    {
+        return $query->where('game_id', $gameID)
+            ->select('selection', DB::raw('COUNT(*) * 100 / SUM(COUNT(*)) OVER() as percentage'))
+            ->groupBy('selection')
+            ->orderByDesc('percentage');
+    }
+
+    public static function scopeHasStarted($query, $gameID, $timestamp)
+    {
+
+        return $query->where('game_id', $gameID)
+            ->where('starts_at', '>', $timestamp)
+            ->exists();
+    }
+
+}
