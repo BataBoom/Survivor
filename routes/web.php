614089cadbf4da8436eae6569fcf5e1f6e03b75c
BataBoom <bataboom@protonmail.com>
2024-05-23 01:01:57 +0000

    Forbidden Pools

--- routes/web.php
+++ routes/web.php
@@ -3,10 +3,15 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\SurvivorController;
 use App\Http\Controllers\PickemController;
+use App\Http\Controllers\PoolController;
+use App\Http\Controllers\ForbiddenController;
+use App\Http\Controllers\HomeController;
+
 use App\Models\Pool;
 use App\Models\SurvivorRegistration;
 use App\Livewire\Fun;
 use App\Livewire\Pickem;
+use App\Livewire\SurvivorGame;
 
 
 /*
@@ -22,17 +27,36 @@ use App\Livewire\Pickem;
 
 Route::view('/', 'welcome');
 
-Route::get('/game/{pool:id}/{survivorregistration:id}', Fun::class)->name('fun.wire');
+Route::get('/forbidden/game/{pool:id}', [ForbiddenController::class, 'show'])->name('forbidden.pool');
 
-Route::get('/pickem/controller/{pool:id}', [PickemController::class, 'show']);
+Route::middleware(['auth', 'survivor'])->group(function () {
 
-Route::get('/pickem/{pool:id}', Pickem::class)->name('pickem.wire');
 
-Route::view('/test', 'test');
+//Route::get('/game/{pool:id}/{survivorregistration:id}', Fun::class)->name('fun.wire');
 
-Route::get('/fun', [SurvivorController::class, 'fun']);
+//Route::get('/survivor/{pool:id}/{survivorregistration:id}', SurvivorGame::class)->name('fun.wire');
+    Route::get('/game/by-pool/{pool:id}', [PoolController::class, 'show'])->name('pool.show')->middleware('can:view,pool');
 
-Route::get('/survivor/pool/{pool:id}', [SurvivorController::class, 'showByPool'])->name('pool.show');
+    Route::get('/game/by-pool/{pool:id}', [PoolController::class, 'show'])->name('pool.show')->middleware('can:view,pool');
+
+    Route::get('/game/pickem/{pool:id}', [PickemController::class, 'show'])->name('pickem.show')->middleware('can:view,pool');
+
+    Route::get('/game/survivor/{pool:id}', [SurvivorController::class, 'showByPool'])->name('survivor.show')->middleware('can:view,pool');
+
+    Route::get('/game/wire/pickem/{pool:id}', Pickem::class)->name('pickem.wire')->middleware('can:view,pool');
+
+    Route::get('/my-pools', [HomeController::class, 'show'])->name('mypools.show');
+
+    Route::view('/test', 'test');
+
+    Route::get('/fun', [SurvivorController::class, 'fun']);
+
+});
+
+
+//Route::get('/pickem/pool/{pool:id}', [PickemController::class, 'showByPool'])->name('pickem.show');
+//Route::get('/pickem/pool/{pool:id}', Pickem::class)->name('pickem.show');
+
 
 Route::view('dashboard', 'dashboard')
     ->middleware(['auth', 'verified'])
