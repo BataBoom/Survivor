9cd20b3f38dbd1f607ac9bfaa1077184a57a04b4
BataBoom <contact@bataboom.dev>
2024-05-23 07:19:04 +0000

    survivor config

---
+++ config/survivor.php
@@ -0,0 +1,10 @@
+<?php
+
+use Illuminate\Support\Facades\Facade;
+use Illuminate\Support\ServiceProvider;
+use Carbon\Carbon;
+
+return [
+    'start_date' => Carbon::createFromDate(2024, 9, 5, 'America/New_York'),
+
+];
