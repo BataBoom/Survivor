30c12d347b8e99e3431567149315bf3a20947752
BataBoom <bataboom@protonmail.com>
2024-05-29 02:17:16 +0000

    Add Admin Pass to ENV/CONFIG for Seeding Purposes

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,8 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\Pool;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
 
 
 class DatabaseSeeder extends Seeder
@@ -18,6 +20,10 @@ class DatabaseSeeder extends Seeder
             WagerTeamsSeeder::class,
             SurvivorScheduleSeeder::class,
         ]);
+
+        Pool::factory(['name' => 'NBZ Pickem', 'entry_cost' => 0])->pickem()->create();
+
+        User::create(['name' => 'admin', 'email' =>  'admin@satoshicast.net', 'password' =>  Hash::make(config('survivor.admin_pw'))]);
 
         //Global Pool
 
