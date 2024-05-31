72948c13abd2d960feac34b99cf1d9c18ac591c6
BataBoom <bataboom@protonmail.com>
2024-05-31 11:29:11 +0000

    V2?

--- resources/views/my-pools.blade.php
+++ resources/views/my-pools.blade.php
@@ -22,7 +22,7 @@
         </div>
         @endsession
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
-            
+
             <div class="flex flex-col justify-center mt-16">
                 <div class="overflow-x-auto">
                     <table class="table">
