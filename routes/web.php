b0adf0e6ddb036ac90c7008a606f7466bfd54fb2
BataBoom <bataboom@protonmail.com>
2024-05-31 10:27:27 +0000

    Resolved merge conflicts after applying stash

--- routes/web.php
+++ routes/web.php
@@ -41,15 +41,11 @@ Route::post('/email/verification-notification', function (Request $request) {
 })->middleware(['auth', 'throttle:6,1'])->name('verification.send');
 */
 
-
 Route::view('/trophy-room', 'trophy')->name('trophies.index');
 Route::view('/faq', 'faq')->name('faq.index');
 Route::view('/landing', 'landing');
 
-
-Route::view('/trophy-room', 'trophy')->name('trophies.index');
-Route::view('/faq', 'faq')->name('faq.index');
-Route::view('/landing', 'landing');
+Volt::route('/loginv2', 'loginv2')->name('loginv2');
 
 Route::view('/', 'welcome');
 
@@ -61,16 +57,13 @@ Route::get('/my-pools/create', [PoolController::class, 'create'])->name('pool.cr
 Route::get('/my-pools/{pool:id}/delete', [PoolController::class, 'destroy'])->name('pool.destroy');
 Route::post('/my-pools/create', [PoolController::class, 'store'])->middleware('auth')->name('pool.post');
 Route::get('/my-pools/register/{pool:id}', [PoolController::class, 'register'])->name('pool.register')->middleware('auth');
+Route::post('/my-pools/register/{pool:id}/checkout', [PoolController::class, 'checkout'])->name('pool.checkout')->middleware('auth');
+
+Route::get('/my-pools/{pool:id}/setup', [PoolController::class, 'finishSetup'])->name('pool.setup');
+Route::post('/my-pools/{pool:id}/setup/checkout', [PoolController::class, 'creatorCheckout'])->name('pool.creatorCheckout')->middleware('auth');
+
+
 Route::get('/my-pools/leave/{survivorregistration:id}', [PoolController::class, 'leave'])->name('pool.leave')->middleware('auth');
-
-Route::get('/my-payments', [PaymentController::class, 'index'])->name('my-payments.index')->middleware('auth');
-
-Route::get('/support', [ContactController::class, 'index'])->name('support.index')->middleware('auth');
-Route::get('/support/{ticket:id}', [ContactController::class, 'show'])->name('support.show')->middleware('auth');
-Route::post('/support/{ticket:id}/store', [ContactController::class, 'store'])->name('ticket.store')->middleware('auth');
-Route::get('/support/{ticket:id}/destroy', [ContactController::class, 'destroy'])->name('ticket.destroy')->middleware('auth');
-Route::post('/support/create', [ContactController::class, 'create'])->name('ticket.create')->middleware('auth');
-//Route::get('/support', [ContactController::class, 'index'])->name('support.index')->middleware('auth');
 
 Route::get('/my-payments', [PaymentController::class, 'index'])->name('my-payments.index')->middleware('auth');
 
