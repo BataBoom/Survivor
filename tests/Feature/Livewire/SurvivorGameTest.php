182656976635244b99f59c43e737f8cf2a897e47
BataBoom <bataboom@protonmail.com>
2024-06-07 09:55:54 +0000

    livewire test

--- tests/Feature/Livewire/SurvivorGameTest.php
+++ tests/Feature/Livewire/SurvivorGameTest.php
@@ -2,37 +2,86 @@
 
 namespace Tests\Feature\Livewire;
 
-use App\Livewire\SurvivorGame;
 use App\Models\Pool;
 use App\Models\SurvivorRegistration;
 use App\Models\User;
+use App\Rules\BeforeWagerQuestionStart;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Livewire\Livewire;
 use Tests\TestCase;
+use App\Livewire\SurvivorGame;
+use App\Models\WagerQuestion;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\Log;
 
 class SurvivorGameTest extends TestCase
 {
-    use RefreshDatabase;
+    //use RefreshDatabase;
+
+    public $pool;
+
+    public $user;
+
+    public $ticket;
+
+    public $week = 1;
 
     protected function setUp(): void
     {
         parent::setUp();
+
+        //Fetch pool
+        $this->pool = Pool::Factory()->survivor()->create();
+
+        $this->user = $this->pool->users->random();
+
+        $this->ticket = SurvivorRegistration::Where('pool_id', $this->pool->id)->where('user_id', $this->user->id)->first();
+
     }
 
     /** @test */
-    public function renders_successfully()
+    public function test_renders_successfully()
     {
-        $user = User::Factory()->create();
-        $pool = Pool::Factory()->single()->create();
-        $registration = SurvivorRegistration::Factory(['user_id' => $user->id, 'pool_id' => $pool->id])->create();
-
-        Livewire::actingAs($user)
-        ->test(SurvivorGame::class, ['mypool' => $pool->id])
+        //$ticket = SurvivorRegistration::Where('pool_id', $this->pool->id)->where('user_id', $this->user->id)->first();
+        Livewire::actingAs($this->user)
+        ->test(SurvivorGame::class, ['pool' => $this->pool])
         ->assertStatus(200)
-        ->assertNotSetStrict('currentPool', $pool)
-        ->assertSetStrict('status', true)
-        ->assertSet('week', 1)
-        ->assertSet('mypool', $pool->id);
+        ->assertSetStrict('week', 1)
+        ->assertSetStrict('realWeek', 1);
+        //->assertSetStrict('survivor', $ticket);
     }
+
+    public function test_submit_pick()
+    {
+
+        $randomWager = WagerQuestion::withWhereHas('gameoptions')
+            ->where('week', $this->week)
+            ->inRandomOrder()
+            ->first();
+
+        $randomSelection = $randomWager->gameoptions->first();
+
+        Livewire::actingAs($this->user)
+            ->test(SurvivorGame::class, ['pool' => $this->pool])
+            ->assertStatus(200)
+            ->assertSetStrict('week', 1)
+            ->assertSetStrict('realWeek', 1)
+            ->assertSee('currentTimeEST', Carbon::now())
+            ->set('selectTeam', $randomSelection->team_id)
+            ->set('selectTeamName', $randomSelection->option)
+            //cant figure out how to test Rule Class BeforeWagerQuestionStart, but passing validation rules here seems fine..
+            ->assertHasNoErrors(['selectTeam' => ['required', 'integer']])
+            ->assertHasNoErrors('selectTeamName')
+            ->call('submit')
+            ->assertDontSeeHtml('<option class="text-white text-xl" value="28">\n'.$randomSelection->option.'\n</option>\n')
+            ->assertDontSeeHtml('<li class="row mb-2 text-center">No selections made, select a team for week 1</li>');
+
+            $this->assertNotEmpty([$this->ticket->survivorPicks]);
+
+            //log::info($this->ticket->survivorPicks);
+
+    }
+
+
 }
