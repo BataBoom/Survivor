446cca7b6cd4bf3d9b441c8adae601461633ca1f
BataBoom <contact@bataboom.dev>
2024-08-14 09:14:34 +0000

    Add team Tie/Draw and some little seeder stuff

--- resources/views/faq.blade.php
+++ resources/views/faq.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-200 leading-tight">
-            Trophy Room | Survivor Legends
+            Frequently Asked Questions
         </h2>
     </x-slot>
 
