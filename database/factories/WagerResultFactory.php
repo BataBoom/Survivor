aae0bce19f1bf133dc7604a61637aeabf95c22e2
BataBoom <bataboom@protonmail.com>
2024-08-16 22:27:37 +0000

    Fix tied result

--- database/factories/WagerResultFactory.php
+++ database/factories/WagerResultFactory.php
@@ -6,6 +6,7 @@ use App\Models\WagerResult;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
 use App\Models\WagerQuestion;
+use Illuminate\Support\Facades\Log;
 
 class WagerResultFactory extends Factory
 {
@@ -14,7 +15,7 @@ class WagerResultFactory extends Factory
     public function definition()
     {
         // Use the state variable 'game' if it exists, otherwise use a random game
-        $fetchGame = $this->state['game'] ?? WagerQuestion::WhereDoesntHave('result')->get()->random();
+        $fetchGame = WagerQuestion::WhereDoesntHave('result')->get()->random();
 
         // Ensure $fetchGame is not null before proceeding
         if ($fetchGame) {
@@ -51,9 +52,24 @@ class WagerResultFactory extends Factory
 
     public function game(int $game)
     {
+
+        $game = WagerQuestion::WhereDoesntHave('result')->Where('game_id', $game)->firstOrFail();
+
         return $this->state(function (array $attributes) use ($game) {
+
+            if ($game->gameoptions->first()->home_team) {
+                $scores = ['home' => rand(21, 48), 'away' => rand(7, 20)];
+            } else {
+                $scores = ['away' => rand(21, 48), 'home' => rand(7, 20)];
+            }
+
             return [
-                'game' => $game,
+                'game' => $game->game_id,
+                'winner' => $game->gameoptions->first()->team_id,
+                'winner_name' => $game->gameoptions->first()->option,
+                'week' =>  $game->week,
+                'home_score' => $scores['home'],
+                'away_score' => $scores['away'],
             ];
         });
     }
